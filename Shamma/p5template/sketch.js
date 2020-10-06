1d84c50a51dd2fe039cf9128d3328736db049bc8
shamma-am <almulla.shamma@gmail.com>
2020-10-06 16:24:15 +0400

    update

---
+++ Shamma/p5template/sketch.js
@@ -0,0 +1,16 @@
+// TenThousandX Assignment: // Add to this code to create a drawing // Note: do not delete anything 
+
+
+function setup() {
+
+   
+    let canvas = createCanvas(800, 400)
+    canvas.parent('p5container');
+    background('#000000')
+nofill()
+stroke('red')
+strokeWeight(3)
+    rect(50,50,700,300)
+
+
+    
