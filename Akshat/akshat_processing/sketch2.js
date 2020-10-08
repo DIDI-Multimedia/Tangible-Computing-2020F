40f70a3a5714afb71e607a5ba82ce93df25cfc70
Akshat-Saraswat <49266518+Akshat-Saraswat@users.noreply.github.com>
2020-10-09 00:01:38 +0530

    Update sketch2.js

---
+++ Akshat/akshat_processing/sketch2.js
@@ -0,0 +1,68 @@
+<<<<<<< Updated upstream
+=======
+/// for future reference:- 
+/// push() starts drawing settings
+/// pop() ends it
+/// push and pop can be nested. They will derive settings in a heirarchy similar to the way a div would
+/// newMaterial gives the spheres a gradient 
+/// 
+function setup() {
+	createCanvas(1000, 1000, WEBGL);
+  //canvas.parent('p5container')
+}
+
+function draw() {
+	let h = hour()
+	h = h + 4
+	let m = minute()
+	let s = second()
+	background('black')
+	
+	// seconds
+	push()
+	  normalMaterial()
+	  rotateZ(TWO_PI / 720 * frameCount)
+	  rotateX(TWO_PI / 720 * frameCount)
+	 for (var i = 0; i < s; i++) {
+		 //s
+        push()
+        rotateZ(TWO_PI / 60 * i)
+        translate(300, 0, 0)
+        sphere(8)
+        pop()
+
+    }
+	pop()
+	
+	//minutes
+	push()
+	  normalMaterial()
+	  rotateX(TWO_PI / 720 * frameCount)
+	 rotateZ(TWO_PI / 720 * frameCount)
+	for (i = 0; i < m; i++){
+		//m
+		push()
+		rotateZ(TWO_PI / 60 * i)
+		translate(200,0,0)
+		sphere(9)
+		pop()
+	}
+	pop() 
+	
+	
+	//seconds
+	push()
+	  normalMaterial()
+	  rotateY(TWO_PI / 720 * frameCount)
+    rotateZ(TWO_PI / 720 * frameCount)
+	for(i = 0; i < h; i++){
+		push()
+		rotateZ(TWO_PI / 24 * i)
+		translate(100,0,0)
+		sphere(14)
+		pop()
+	}
+pop()
+	
+}
+>>>>>>> Stashed changes
