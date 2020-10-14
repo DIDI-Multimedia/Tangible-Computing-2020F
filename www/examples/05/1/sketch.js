ef6bba633ff8ad54048779d464356f62f797cb94
Sayjel Vijay Patel <sayjel.v.patel@gmail.com>
2020-10-14 08:55:36 +0400

    updates

--- www/examples/05/1/sketch.js
+++ www/examples/05/1/sketch.js
@@ -1,7 +1,7 @@
-let names = ['Sayjel', 'Waleed', 'Akshat', 'Zaade', 'Mariam', 'Hussien', 'Arnav', 'Agma','Mo','Fatima', 'Sharma','Mariam Lambertt']
+let names = ['Sayjel', 'Waleed', 'Akshat', 'Zaade', 'Mariam', 'Hussien', 'Arnav', 'Agma', 'Mo', 'Fatima', 'Sharma', 'Mariam Lambertt']
 let verbs = ['walks', 'feeds', 'pets', 'washes', 'plays with']
-let animals = ['cat', 'dog', 'lion', 'fish', 'Eagle', 'quokka', 'horse', 'cat','birds', 'horse']
-let cols =['matte white', 'beige', 'beige', 'blue', 'green', 'red', 'purple ']
+let animals = ['cat', 'dog', 'lion', 'fish', 'Eagle', 'quokka', 'horse', 'cat', 'birds', 'horse']
+let cols = ['matte white', 'beige', 'beige', 'blue', 'green', 'red', 'purple ']
 
 let sentences = []
 
@@ -14,30 +14,29 @@ function setup() {
     canvas.parent('p5container')
     background(0)
 
-    for (var i = 0; i < names.length; i++){
+    for (var i = 0; i < names.length; i++) {
 
         let sentence = names[i] + " " + random(verbs) + ' their ' + random(animals)
         sentences.push(sentence)
 
     }
-  
+
 }
 
 function draw() {
 
     background(255)
-    textAlign(CENTER,CENTER)
-    textSize(32)
+    textAlign(CENTER, CENTER)
+    textSize(16)
 
 
-    for (var i = 0; i < sentences.length; i++){
+    for (var i = 0; i < sentences.length; i++) {
 
-    fill(i*25,255-i*30,i*40)
+        fill(i * 25, 255 - i * 50, i * 10)
         let sentence = sentences[i]
-
-        let posX = cos(frameCount/(100 + sentences.length*i))*width/4
-     let posY = sin(frameCount/(100*i + sentences.length))*width/4
-        text(sentences[i], posX+width/2,posY+height/2)
+        let posX = cos(frameCount / (100 + sentences.length * i)) * width / 4
+        let posY = sin(frameCount / (100 * i + sentences.length)) * width / 4
+        text(sentences[i], posX + width / 2, posY + height / 2)
 
 
 
@@ -48,4 +47,4 @@ function draw() {
 
 
 
-} 
+}
