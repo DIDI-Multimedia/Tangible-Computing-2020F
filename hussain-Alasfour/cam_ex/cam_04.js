6f8aab67b01598db75a4767253966a1a806a0511
HussainA-1 <71366401+HussainA-1@users.noreply.github.com>
2020-10-24 19:00:29 +0400

    update-1

---
+++ hussain-Alasfour/cam_ex/cam_04.js
@@ -0,0 +1,56 @@
+
+
+let capture
+
+function setup() {
+
+    
+    let canvas = createCanvas(600, 600)
+    canvas.parent('p5container')
+    capture = createCapture()
+    capture.hide()
+
+}
+
+function draw() {
+
+    background(218, 208, 192)
+    background(capture.get())
+    image(capture,0,0)
+    filter(INVERT);
+
+    let step = 35
+
+
+    textAlign(CENTER, CENTER)
+
+    for (var i = 0; i < capture.width; i += step) {
+
+        for (var j = 0; j < capture.height; j += step) {
+
+            let col = capture.get(i, j)
+            let val = brightness(col)
+
+            if (val < 50) {
+
+                fill(val)
+                textSize(step)
+                rect(i, j, step, step)
+                noStroke()
+
+                text(val, i, j)
+
+            } else {
+
+                fill(43, 42, 92,75)
+                rect(i, j, step, step)
+            }
+
+
+
+
+
+
+        }
+    }
+}
