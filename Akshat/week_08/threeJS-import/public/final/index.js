a2ff6b0580c84846e3899c448a4e1fa096914b44
Akshat-Saraswat <49266518+Akshat-Saraswat@users.noreply.github.com>
2020-12-15 01:19:25 +0400

    final-v2

--- Akshat/week_08/threeJS-import/public/final/index.js
+++ Akshat/week_08/threeJS-import/public/final/index.js
@@ -17,7 +17,9 @@ function draw() {
   fill(140,0,211);
   ellipse( windowWidth/2 , windowHeight/6 * 1 , 100, 100);
   ellipse( windowWidth/2 , windowHeight/6 * 2 , 100, 100);
-
+  ellipse( windowWidth/2 , windowHeight/6 * 3 , 100, 100);
+  ellipse( windowWidth/2 , windowHeight/6 * 4 , 100, 100);
+  ellipse( windowWidth/2 , windowHeight/6 * 5 , 100, 100);
   
 
 
@@ -28,12 +30,26 @@ function mousePressed(  ) {
   // Check if mouse is inside the circle
   let d = dist(mouseX, mouseY, windowWidth/2, windowHeight/6 * 1);
   let e = dist(mouseX, mouseY, windowWidth/2, windowHeight/6 * 2);
+  let f = dist(mouseX, mouseY, windowWidth/2, windowHeight/6 * 3);
+  let g = dist(mouseX, mouseY, windowWidth/2, windowHeight/6 * 4);
+  let h = dist(mouseX, mouseY, windowWidth/2, windowHeight/6 * 5);
  
   if (d < 100) {
    window.location.href = "../ar-import.html"
   }
 
   if(e<100){
-    window.location.href = "../custom-patterns.html.js"
+    window.location.href = "../ar-import2.html"
+  }
+  
+  if(f<100){
+    window.location.href = "../ar-import3.html"
+  }
+
+  if(g<100){
+    window.location.href = "../ar-import5.html"
+  }
+  if(h<100){
+    window.location.href = "../ar-import6.html"
   }
 }
